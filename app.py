4bf686475969a79b951f5c23fa8f3130225064cd
MohamadHajjRabee <a39630595@gmail.com>
2024-04-17 18:18:55 +0300

    removed useless lines

--- app.py
+++ app.py
@@ -23,28 +23,14 @@ def predict():
     try:
         request_data = request.json
         base64_image = request_data['image']
-
         image_bytes = base64.b64decode(base64_image)
-        # image = Image.open(BytesIO(image_bytes)).convert("RGB")
-
-        # img_array = np.array(image)
-        # img = Image.fromarray(img_array)
-        # resized_image = img.resize((224, 224))
-        # img_array = tf.keras.utils.img_to_array(resized_image)
-        # img_array = np.expand_dims(img_array, axis=0)
-        # img_array /= 255.0
-
-        # tensor = tf.convert_to_tensor(img_array)
         img = image.load_img(BytesIO(image_bytes), target_size=(224, 224))
         img_array = image.img_to_array(img)
         img_array = np.expand_dims(img_array, axis=0)
         img_array /= 255.0
 
-        interpreter.set_tensor(interpreter.get_input_details()[0]["index"],
-                               img_array)
-
+        interpreter.set_tensor(interpreter.get_input_details()[0]["index"], img_array)
         interpreter.invoke()
-
         output = interpreter.tensor(interpreter.get_output_details()[0]['index'])
         result = output()[0]
         predicted_class = np.argmax(result)
